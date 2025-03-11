b4e76051642e2fcb13cbda173161cf2cdf8241f0
Mani-Jebraeily <mani.jebraeily1382@gmail.com>
2025-03-11 18:24:26 +0330

    done

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base:"crypto-app",
+  base:"/crypto-app/",
   plugins: [react(), tailwindcss()],
 })
